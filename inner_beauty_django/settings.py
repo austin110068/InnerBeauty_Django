29474eace5a0876e038db872e90f0c1ad6540d5e
Chien-Yu Chen <austin110068@gmail.com>
2021-09-09 01:40:12 -0700

    Add ALLOWED_HOSTS

--- inner_beauty_django/settings.py
+++ inner_beauty_django/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['inner-beauty-api.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['inner-beauty-api.herokuapp.com', 'inner-beauty.herokuapp.com', '127.0.0.1']
 
 # Stripe
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
