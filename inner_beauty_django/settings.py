6f14974327d13c09ca1377292caa9aaacdaaa6ab
Chien-Yu Chen <austin110068@gmail.com>
2021-09-09 11:58:42 -0700

    Add staticfiles dirs

--- inner_beauty_django/settings.py
+++ inner_beauty_django/settings.py
@@ -153,6 +153,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
