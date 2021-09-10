523f9d51e2bb78936db4153af7291dd2fce48a2e
Chien-Yu Chen <austin110068@gmail.com>
2021-09-09 17:11:05 -0700

    Add template dirs

--- inner_beauty_django/settings.py
+++ inner_beauty_django/settings.py
@@ -157,6 +157,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
 
 
 MEDIA_URL = '/media/'
